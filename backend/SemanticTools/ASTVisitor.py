184e34c36d81540e87257a87eb70003656028541
JiArcangel0916 <arcangelalbrecht@gmail.com>
2026-02-24 13:46:52 +0800

    added notes each semantic tools

--- backend/SemanticTools/ASTVisitor.py
+++ backend/SemanticTools/ASTVisitor.py
@@ -1,3 +1,12 @@
+########## ASTVisitor #########
+# Base class for for the ASTTraverser stating how to "visit" each node
+# Each visit has its own way of traversing a node
+# vist() - uses getattr() of python to find the correct function that visits a specific node
+# generic_visit() - visits the root node or the topmost in the tree. This prevents the compiler from cashing if no function has made for a node
+# visit_children() - while a node has a children, it visits its children until to the leaf node
+
+from SymbolTable import SymbolTable
+
 class ASTVisitor:
     def visit(self, node, parent=None):
         visitor = getattr(self, f'visit_{type(node).__name__}', self.generic_visit)
@@ -11,5 +20,14 @@ class ASTVisitor:
         for child in node.children:
             self.visit(child, node)
 
+########## ASTTraverser ##########
+# Main component of the semantic analyzer
+# It defines the meaning of the nodes by type checking
+# Puhsing and popping variables to the symbol table and checks if a variable exists
+# It determines the result of an expression
+
 class ASTTraverser(ASTVisitor):
-    pass
+    def __init__(self, symbol_table):
+        self.symbol_table = symbol_table
+        self.errors = []
+        self.unresolved = []
--- backend/SemanticTools/SymbolTable.py
+++ backend/SemanticTools/SymbolTable.py
@@ -1,26 +1,32 @@
+########## SYMBOL TABLE ##########
+# The symbol table keeps track of all the variables declared and called in the program
+# It stores declared variables and checks whether one is already declared or not
+# It differentiates variabels based on their scope
+# This also remembers the values stored in variables
+
 class SymbolTable:
     def __init__(self):
-        self.scopes = [{}]
+        self.scopes = [{}]          # Keeps track of a variables accesibility as local or global
 
     def push(self):
-        self.scopes.append({})
+        self.scopes.append({})      # Creates a new scope level
 
     def pop(self):
         self.scopes.pop()
 
-    def get(self, name):
+    def get(self, name):            # Searches for the variable called from the innermost scope
         for scope in reversed(self.scopes):
             if name in scope:
                 return scope[name]
         return None
     
-    def get_local(self, name):
+    def get_local(self, name):      # Only checks the if the variable called is a local variable
         if name in self.scopes[-1]:
             return self.scopes[-1][name]
         print(f'{name} not in local scope')
         return None
     
-    def get_type(self, name):
+    def get_type(self, name):       # Gets the data type of the variable called
         for scope in reversed(self.scopes):
             if name in scope:
                 if hasattr(scope[name], 'datatype'): return scope[name].datatype
@@ -28,7 +34,7 @@ class SymbolTable:
             print(f'{name} not found')
             return None
         
-    def set(self, name, value):
+    def set(self, name, value):     # Handles declaration, assignment, and reassignment, if variable does not exist, it creates one
         for scope in reversed(self.scopes):
             if name in scope:
                 scope[name] = value
@@ -38,5 +44,5 @@ class SymbolTable:
             self.scopes.append({})
         self.scopes[-1][name] = value
 
-    def set_local(self, name, value):
+    def set_local(self, name, value):   # Puts the variable in the current scope even if the same identifier exists
         self.scopes[-1][name] = value
